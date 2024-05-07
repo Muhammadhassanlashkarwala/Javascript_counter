cc6bf9ba26845f2e9610725e0c8edfba4c8ef830
hassan memon <bhaihassan036@gmail.com>
2024-05-07 22:20:43 +0100

    third

---
+++ script.js
@@ -0,0 +1,11 @@
+let value = 0
+function aDDing() {
+    value++
+    let Plus = document.getElementById("increment").innerText = value
+    console.log(Plus);
+}
+
+function rEset() {
+    let reset = document.getElementById("increment").innerText = "0"
+    console.log(reset);
+}
