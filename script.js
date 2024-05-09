ac3a6218fb1514c1579b4eedd956bee07086a129
hassan memon <bhaihassan036@gmail.com>
2024-05-09 10:37:25 +0100

    nineth

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let value = 0
 function aDDing() {
     value++
-    let Plus = document.getElementById("increment").innerText = value
+    let Plus = document.getElementById("title").innerText = value
     console.log(Plus);
 }
 
 function rEset() {
-    let reset = document.getElementById("reset").innerText = "0"
+    let reset = document.getElementById("title").innerText = "0"
     console.log(reset);
 }
 
 function sUbtracting() {
     value--
-    let subtract = document.getElementById("decrement").innerText = value
+    let subtract = document.getElementById("title").innerText = value
     console.log(subtract);
 }
