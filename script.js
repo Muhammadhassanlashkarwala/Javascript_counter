074294088e8a2c015f4a75374c547ba67298125a
hassan memon <bhaihassan036@gmail.com>
2024-07-01 03:52:11 +0500

    align

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let value = 0
 function aDDing() {
     value++
-    let Plus = document.getElementById("title").innerText = value
+    let Plus = document.getElementById("title").innerHTML = value
     console.log(Plus);
 }
 
 function rEset() {
-    let reset = document.getElementById("title").innerText = "0"
+    let reset = document.getElementById("title").innerHTML = "0"
     console.log(reset);
 }
 
 function sUbtracting() {
     value--
-    let subtract = document.getElementById("title").innerText = value
+    let subtract = document.getElementById("title").innerHTML = value
     console.log(subtract);
 }
